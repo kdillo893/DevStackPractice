4224b6065a7cfd88ff9a49131b85ec36d1a6502d
Kevin Dillon <kdillo893@gmail.com>
2023-08-28 21:50:38 -0500

    Moving around things, more testing

--- src/main/java/com/kdillo/simple/SimpleApp.java
+++ src/main/java/com/kdillo/simple/SimpleApp.java
@@ -1,5 +1,6 @@
 package com.kdillo.simple;
 
+import com.kdillo.simple.db.PostgresqlConnectionProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +11,6 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.sql.SQLException;
 import java.util.Properties;
 
 import java.sql.*;
@@ -18,137 +18,89 @@ import java.util.UUID;
 
 public class SimpleApp {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger(SimpleApp.class);
 
-    static final String ALPHANUMERIC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
+    private static final String ALPHANUMERIC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     //initialize a random number generator when the app launches
-    static SecureRandom rnd = new SecureRandom();
+    public static final SecureRandom rnd = new SecureRandom();
 
-    static Properties props = null;
+    private static Properties props = null;
 
     public static void main(String[] args){
 
         try {
-            getApplicationProperties();
-            DummyRun();
-        } catch (SQLException exs) {
-            exs.printStackTrace();
+            loadApplicationProperties();
+
+//            PGSimpleDataSource ds = new PGSimpleDataSource();
+            PostgresqlConnectionProvider pgConProvider = new PostgresqlConnectionProvider(props);
+
+            DummyRun(pgConProvider);
+
+            //main loop
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-        return;
-
-        //load app properties from config file.
-//        try (InputStream configFileStream = SimpleApp.class.getClassLoader().getResourceAsStream("config.properties")) {
-//            if (configFileStream == null) {
-//                System.out.println("No props file found");
-//                return;
-//            }
-//            Properties props = new Properties();
-//            props.load(configFileStream);
-//
-//            properties = props;
-//        } catch (IOException ex) {
-//            //unable to load from properties file..
-//            System.out.println("Never loaded props file, " + ex.getMessage());
-//            LOGGER.error(ex.getMessage());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        if (properties == null || properties.isEmpty()) {
-//            //failed to load properties, exit app.
-//            return;
-//        }
-//
-//        DBAccess dbAccess = new DBAccess(properties);
-//        dbAccess.readDataBase();
-//
-//        //spin wait for requests:
-
     }
 
-    public static void DummyRun() throws Exception, SQLException {
+    public static void DummyRun(PostgresqlConnectionProvider pgConProvider) {
         //just a dummy run to see if I can make the database access work.
 
-        //load dbProps from the application properties (db.X)
-        Properties dbProps = new Properties();
-        dbProps.setProperty("user", props.getProperty("db.user"));
-        dbProps.setProperty("password", props.getProperty("db.password"));
+        try (Connection conn = pgConProvider.getConnection()) {
 
-        String url = props.getProperty("db.url") + props.getProperty("db.name") +
-                "?currentSchema=" + props.getProperty("db.schema");
+            //getting a user from the users table, print all the info from the rows returned.
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM users WHERE email = 'kdillo893@gmail.com'");
+            while (rs.next()) {
 
-        //connector, use the db properties
-        Connection conn = DriverManager.getConnection(url, dbProps);
+                //getting values for each of the columns: uid, first_name, last_name, email, email, created, updated, pass_hash, pass_salt.
 
-        //getting a user from the users table, print all the info from the rows returned.
-        Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM users WHERE email = 'kdillo893@gmail.com'");
-        while (rs.next()) {
+                String uid = rs.getString("uid");
+                String fname = rs.getString("first_name");
+                String lname = rs.getString("last_name");
+                String email = rs.getString("email");
+                Date created = rs.getDate("created");
+                Date updated = rs.getDate("updated");
+                String passHash = rs.getString("pass_hash");
+                String passSalt = rs.getString("pass_salt");
 
-            //getting values for each of the columns: uid, first_name, last_name, email, email, created, updated, pass_hash, pass_salt.
+                System.out.printf("uid=%s, fname=%s, lname=%s, email=%s, created=%s, updated=%s, passHash=%s, passSalt=%s\n", uid, fname, lname, email, created, updated, passHash, passSalt);
+            }
 
-            String uid = rs.getString("uid");
-            String fname = rs.getString("first_name");
-            String lname = rs.getString("last_name");
-            String email = rs.getString("email");
-            Date created = rs.getDate("created");
-            Date updated = rs.getDate("updated");
-            String passHash = rs.getString("pass_hash");
-            String passSalt = rs.getString("pass_salt");
+            rs.close();
+            st.close();
 
-            System.out.printf("uid=%s, fname=%s, lname=%s, email=%s, created=%s, updated=%s, passHash=%s, passSalt=%s\n", uid, fname, lname, email, created, updated, passHash, passSalt);
+            //        conn.close();
+            //        conn = createNewDbConnection();
+
+            //testing to update the passHash of my user row with an update statement.rAN
+            //make a password hash out of random salt
+            String newSalt = randomString(32);
+            String passHash = digestMessageWithAlg("silly-Thing123;xD", "SHA512");
+
+            UUID myUid = UUID.fromString("7d95e88a-f10f-4b20-8303-e26db72ddd74");
+            PreparedStatement pst = conn.prepareStatement("update users set pass_hash = ?, pass_salt = ? where uid = ? ;;");
+
+            pst.setString(1, passHash);
+            pst.setString(2, newSalt);
+            pst.setObject(3, myUid);
+
+            //execute update, returns just the number of effected rows.
+            int rowsUpdated = pst.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.printf("rowsUpdated=%d, passHash=%s, passSalt=%s, UUID=%s", rowsUpdated, passHash, newSalt, myUid);
+            }
+
+            rs.close();
+            st.close();
+
+        //can handle sqlexception differently later
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-
-        rs.close();
-        st.close();
-
-//        conn.close();
-
-//        conn = DriverManager.getConnection(url, props);
-        //testing to update the passHash of my user row with an update statement.rAN
-        //make a password hash out of random salt
-        String newSalt = randomString(32);
-        String passHash = digestMessageWithAlg("silly-Thing123;xD", "SHA512");
-
-        UUID myUid = UUID.fromString("7d95e88a-f10f-4b20-8303-e26db72ddd74");
-        PreparedStatement pst = conn.prepareStatement("update users set pass_hash = ?, pass_salt = ? where uid = ? ;;");
-
-
-
-        pst.setString(1, passHash);
-        pst.setString(2, newSalt);
-        pst.setObject(3, myUid);
-
-
-        int rowsUpdated = pst.executeUpdate();
-
-        if (rowsUpdated > 0) {
-            System.out.printf("rowsUpdated=%d, passHash=%s, passSalt=%s, UUID=%s", rowsUpdated, passHash, newSalt, myUid);
-        }
-
-        //what does update return? not sure.
-//        while (rs.next()) {
-//            // just printing the first column, then
-//            System.out.printf("uid=%s", rs.getString("uid"));
-//            System.out.printf("passHash=%s", rs.getString("pass_hash"));
-//            System.out.printf("pass_salt=%s", rs.getString("pass_salt"));
-////            System.out.printf("uid=%s", rs.getString("uid"));
-////            System.out.printf("uid=%s", rs.getString("uid"));
-////            System.out.printf("uid=%s", rs.getString("uid"));
-//            System.out.println();
-//        }
-
-        rs.close();
-        st.close();
-
     }
 
-
-    static String digestMessageWithAlg(String message, String alg) {
+    protected static String digestMessageWithAlg(String message, String alg) {
 
         try {
             MessageDigest md = MessageDigest.getInstance(alg);
@@ -176,14 +128,14 @@ public class SimpleApp {
         return null;
     }
 
-    static String randomString(int len){
+    public static String randomString(int len){
         StringBuilder sb = new StringBuilder(len);
         for(int i = 0; i < len; i++)
             sb.append(ALPHANUMERIC.charAt(rnd.nextInt(ALPHANUMERIC.length())));
         return sb.toString();
     }
 
-    private static void getApplicationProperties() {
+    private static void loadApplicationProperties() {
 
         try (InputStream propsStream = new FileInputStream("src/main/resources/config.properties")) {
             props = new Properties();
---
+++ src/main/java/com/kdillo/simple/db/PostgresqlConnectionProvider.java
@@ -0,0 +1,41 @@
+package com.kdillo.simple.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class PostgresqlConnectionProvider {
+
+    private final String dbUser;
+    private final String dbPassword;
+    private final String dbName;
+    private final String dbSchema;
+    private final String dbHost;
+    private int dbPort = 5432;
+
+    public PostgresqlConnectionProvider(Properties props) {
+        //load dbProps from the application properties (db.X) passed.
+
+        this.dbUser = props.getProperty("db.user");
+        this.dbPassword = props.getProperty("db.password");
+        this.dbName = props.getProperty("db.name");
+        this.dbSchema = props.getProperty("db.schema");
+        this.dbHost = props.getProperty("db.host");
+        this.dbPort = Integer.parseInt(props.getProperty("db.port"));
+
+    }
+
+    public Connection getConnection() throws SQLException {
+
+        Properties dbProps = new Properties();
+        dbProps.setProperty("user", dbUser);
+        dbProps.setProperty("password", dbPassword);
+
+        String url = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/"
+                + dbName + "?currentSchema=" + dbSchema;
+
+        //return connector, use the db properties
+        return DriverManager.getConnection(url, dbProps);
+    }
+}
