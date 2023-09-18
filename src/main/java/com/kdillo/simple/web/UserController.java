6d55fec3a6474406ca8d9588bb91a382536e46d0
Kevin Dillon <kdillo893@gmail.com>
2023-09-18 13:30:03 -0500

    moving things, adding documentation

--- src/main/java/com/kdillo/simple/web/UserController.java
+++ src/main/java/com/kdillo/simple/web/UserController.java
@@ -3,23 +3,18 @@ package com.kdillo.simple.rest;
 import com.kdillo.simple.SimpleApp;
 import com.kdillo.simple.db.UserDBImpl;
 import com.kdillo.simple.entities.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
 public class UserController {
 
     //temp tester finals
     private static final String TEMPLATE = "Hello %s";
     private final AtomicLong COUNTER = new AtomicLong();
 
-    @GetMapping("/user")
-    public UserRecord getUser(@RequestParam(value="uid")UUID uid) {
+    public UserRecord getUser(UUID uid) {
 
         //grab the thing, then put things in the record to return...
         UserDBImpl userDB = new UserDBImpl(SimpleApp.pgConProvider);
