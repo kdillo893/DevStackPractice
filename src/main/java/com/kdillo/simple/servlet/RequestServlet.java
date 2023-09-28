be128506b3358795d7006f3f69474166ebe79366
Kevin Dillon <kdillo893@gmail.com>
2023-09-27 21:34:23 -0500

    fixing path mismatches

--- src/main/java/com/kdillo/simple/servlet/RequestServlet.java
+++ src/main/java/com/kdillo/simple/servlet/RequestServlet.java
@@ -16,9 +16,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-import java.math.BigDecimal;
-import java.util.Enumeration;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
@@ -40,56 +41,100 @@ public class RequestServlet extends HttpServlet {
 
             props.load(propsStream);
             pgConProvider = new PostgresqlConnectionProvider(props);
-            
+
         } catch (IOException ex) {
 //            LOGGER.debug("Unable to load app properties");
             ex.printStackTrace();
         }
     }
 
+    private static String myAppUrl(Properties props) {
+
+        if (props == null || props.isEmpty()) {
+            return null;
+        }
+
+        boolean useSsl = props.getProperty("app.ssl").equalsIgnoreCase("true");
+        String appUrl = useSsl ? "https://" : "http://";
+        appUrl += props.getProperty("app.hostname");
+        appUrl += props.getProperty("app.port") != null ? ":" + props.getProperty("app.port") : "";
+        appUrl += "/";
+
+        return appUrl;
+    }
+    
+    private static List<String> parsePath(String pathInfo) {
+        
+        if (pathInfo == null)
+            return null;
+        
+        List<String> orderedPathParts = new ArrayList<String>();
+        try {
+            for (var pathPart : pathInfo.split("/")) {
+                orderedPathParts.add(URLDecoder.decode(pathPart, "UTF-8"));
+            }
+        } catch (UnsupportedEncodingException ex) {
+            //couldn't parse, return null;
+            return null;
+        }
+        
+        // if the first item is blank (because of pathinfo starting with /), remove it.
+        if (orderedPathParts.get(0).isEmpty())
+            orderedPathParts.remove(0);
+        
+        return orderedPathParts;
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //set allow cors from my own origin:
+        resp.addHeader("AccessControl-Allow-Origin", myAppUrl(props));
+
+        super.service(req, resp);
+    }
+
     /**
      * Process get for business objects which are displayed by UI
+     *
      * @param request
      * @param response
-     * @throws IOException 
+     * @throws IOException
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //parse the URI
+//        List<String> pathParts = parsePath(request.getPathInfo());
+//        System.out.println(pathParts);
+//        System.out.println(request.getRequestURI());
+        
+        //the path could contain many different parts...
+        
+        
+        
         
         //my "get" method will need to parse by "query multiple" or "query by ID"        
-        
         String parmId = request.getParameter("id");
-
-//        if (parmId == null) {
-//            response.setContentType("text/html");
-//
-//            //hello
-//            PrintWriter out = response.getWriter();
-//            out.println("<html><body>");
-//            out.println("<h1>" + "No ID Parm" + "</h1>");
-//            out.println("</body></html>");
-//
-//            return;
-//        }
 
         UUID id = null;
         try {
-            if (parmId != null)
+            if (parmId != null) {
                 id = UUID.fromString(parmId);
+            }
         } catch (IllegalArgumentException ilex) {
-            ilex.printStackTrace();
 
             response.setContentType("text/html");
 
             //hello
             PrintWriter out = response.getWriter();
             out.println("<html><body>");
-            out.println("<h1>" + "Failed to parse uid" + "</h1>");
+            out.println("<h1>" + "Failed to parse id" + "</h1>");
             out.println("</body></html>");
 
             return;
         }
-        
+
         //if ID, then "get by Id", else "query by parms"
         if (id != null) {
             getById(id, request, response);
@@ -100,19 +145,11 @@ public class RequestServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        
-        
         super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        //create or update an entity, these will have body describing the object...
-        
-        
-        
         super.doPut(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
@@ -126,53 +163,59 @@ public class RequestServlet extends HttpServlet {
         pgConProvider = null;
         props = null;
     }
-    
+
     private void getById(UUID id, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        resp.setContentType("text/json");
+        Writer out = resp.getWriter();
 
         String pathInfo = req.getPathInfo();
-        
-        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        if (pathInfo == null || pathInfo.isBlank()) {
+
+            out.write("{}");
+            return;
+        }
+
         if (pathInfo.startsWith("/user")) {
             UserDBImpl userDbImpl = new UserDBImpl(pgConProvider);
             Optional<User> optUser = userDbImpl.getById(id);
-            
+
             jsonBuilder.add("user", optUser.isPresent() ? optUser.get().toJson() : JsonObject.NULL);
         }
-        
-        resp.setContentType("text/json");
-        
-        Writer out = resp.getWriter();
+
         out.write(jsonBuilder.build().toString());
-        
     }
 
     private void getMultiple(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
-        String pathInfo = request.getPathInfo();
-        
-        //get request means that we must have this as parameters, not body
-        
+
         JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        response.setContentType("text/json");
+        Writer out = response.getWriter();
+
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null || pathInfo.isBlank()) {
+
+            out.write("{}");
+            return;
+        }
+
         if (pathInfo.startsWith("/users")) {
             //query by what? just last name for now
-            
+
             User queryUser = new User();
             queryUser.setLastName(request.getParameter("last"));
-            
+
             UserDBImpl userDbImpl = new UserDBImpl(pgConProvider);
             List<User> users = userDbImpl.getAll(queryUser);
-            
+
             JsonArrayBuilder userJsonArray = Json.createArrayBuilder();
             for (User user : users) {
                 userJsonArray.add(user.toJson());
             }
-            
+
             jsonBuilder.add("users", userJsonArray);
         }
-        
-        response.setContentType("text/json");
-        
-        Writer out = response.getWriter();
+
         out.write(jsonBuilder.build().toString());
     }
 }
