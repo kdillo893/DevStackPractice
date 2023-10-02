85bd212704c3783b660ba50b89b26584c16d1bc2
Kevin Dillon <kdillo893@gmail.com>
2023-10-02 16:15:22 -0500

    messing around, nothing special

---
+++ src/main/java/com/kdillo/simple/annotations/MyTestAnnotation.java
@@ -0,0 +1,16 @@
+package com.kdillo.simple.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author kdill
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface MyTestAnnotation {
+    
+}
