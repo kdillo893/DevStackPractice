2a832b80147e1b5ea67e989c10e0c0b128543dfb
Kevin Dillon <kdillo893@gmail.com>
2023-11-08 18:45:01 -0600

    retrieve users from table; proxy path to my API

--- src/main/ui/app/users/page.js
+++ src/main/ui/app/users/page.js
@@ -1,30 +1,66 @@
 'use client'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-
-export default function() {
-    //grab the list of users from my endpoint
-    const [users, setUsers] = useState([]);
-
-    axios.get('http://localhost:8080/simplest/api/users/')
-        .then( (response) => {
-            //response has "success" which is an array with one object.
-            const data = response.success[0].data;
-            setUsers(data.users);
-            console.log(response);
-
-        }).catch( (error) => {
-            console.log(error);
-        });
+function UserRow({ uid, first_name, last_name, email}) {
 
     return (
-        <ol>
-        {
-            users.map( (idx) => {
-                return <li>{idx}</li>
-            })
-        }
-        </ol>
+        <TableRow>
+            <TableCell>{uid}</TableCell>
+            <TableCell>{first_name}</TableCell>
+            <TableCell>{last_name}</TableCell>
+            <TableCell>{email}</TableCell>
+            <TableCell>Notes</TableCell>
+        </TableRow>
+    )
+}
+
+export default function() {
+    const [users, setUsers] = useState([]);
+    const [usersLoaded, setUsersLoaded] = useState(false);
+
+    //this happens on initial load (basically componentDidMount)
+    useEffect(() => {
+        if (users == null || users.length == 0)
+        axios.get('/api/users/')
+            .then( (response) => {
+                //response has "success" which is an array with one object.
+                const data = response.data.success[0].data;
+
+                if (data != null) setUsers(data.users);
+
+                setUsersLoaded(true);
+            }).catch( (error) => {
+                console.log(error);
+            });
+    });
+
+
+    if (!usersLoaded) return <p>Loading ...</p>;
+    if (!users || users.length == 0) return <p>No users</p>;
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>UID</TableCell>
+                        <TableCell>First Name</TableCell>
+                        <TableCell>Last Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Notes</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        users?.map( (aUser) => {
+                            console.log(aUser.uid);
+                            return <UserRow {... aUser} />
+                        })
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
